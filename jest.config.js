83e4a43ff9501b668c92aba85ba742718fa5e4e7
hamid <mansouri.hamid23@gmail.com>
2026-05-29 12:42:02 +0200

    change coverage

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,10 @@ module.exports = {
     collectCoverageFrom: ["src/**/*.js"],
     coverageThreshold: {
         global: {
-            branches: 90,
-            functions: 90,
-            lines: 90,
-            statements: 90
+            branches: 70,
+            functions: 70,
+            lines: 70,
+            statements: 70
         }
     }
 }   
